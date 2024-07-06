46eb8d9ee4eaff2804e31de43e3417cebe5a4a0f
vamsikrishna020 <1709vamsikrishna@gmail.com>
2024-07-06 13:37:10 +0530

    Update BCD_adder.v

--- BCD_adder.v
+++ BCD_adder.v
@@ -8,16 +8,17 @@ wire c;
 
 assign {c , p}= a+b;
 
-always @*
+	always @(*)
 	begin 
 	if((~c & (p>9))|(c & (p<=9)))begin  
 	// if sum<=9 and carry =1 
 	// or sum>9 and carry =0 
 	// then final answer is incorrect // add 6 to it
 
-		 assign {carry,out}= p +6;
+		 {carry,out}= p +6;
 		
-	end else begin
+	end 
+		else begin
 	
 		carry=c;
 		out=p;
