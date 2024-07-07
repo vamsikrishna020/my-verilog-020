ac634d4df4d340e937f63df89faf16f55f34e028
vamsikrishna020 <1709vamsikrishna@gmail.com>
2024-07-07 12:15:37 +0530

    Update BCD_adder_TB.v

--- BCD_adder_TB.v
+++ BCD_adder_TB.v
@@ -1,23 +1,24 @@
-`include "BCD_adder.v"
 module BCD_adder_tb ;
 
 reg [7:0] a,b;
-wire [7:0] out;
+  wire [7:0] sum;
 wire carry ;
 
-BCD_adder u0(.a(a),.b(b),.out(out),.carry(carry));
+  BCD_adder u0(.a(a),.b(b),.sum(sum),.carry(carry));
 
 initial
 begin
 
-$monitor("a=%b b=%b carry=%b out=%b",a,b,carry,out);
+  $monitor("a=%b b=%b carry=%b out=%d%d",a,b,carry,sum[7:4],sum[3:0]);
 
-#5;
 a=8'b01010111;
 b=8'b00100110;
 #5;
 a=8'b00001000;
 b=8'b00001001;
+ #5;
+ a = 8'b01010101;
+ b = 8'b01010000;
 end
 
 endmodule
